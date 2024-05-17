1e92fe12304cc00d6c6a4cd1cd9713e58f442c08
ouzgithub <ousmanebalnde1305@gmail.com>
2024-05-17 10:57:35 +0000

    first commit

---
+++ TrainingSpringBoot/src/main/java/man/SpringBoot/TrainingSpringBoot/TrainingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package man.SpringBoot.TrainingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringBootApplication.class, args);
+	}
+
+}
